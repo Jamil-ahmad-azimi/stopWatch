75f806155dbc49d900e85f7fbd024408f6139873
jamil-ahmad-azimi <jamilazimi11@gmail.com>
2025-04-28 10:51:58 -0700

    The HTML and CSS and JS Files Are uploded By jamil Ahmad

---
+++ script.js
@@ -0,0 +1,39 @@
+let [seconds, minutes, hours] = [0, 0, 0];
+let time = document.querySelector(".displayTime");
+let timer = null;
+
+function stopwatch() {
+    seconds++;
+    if (seconds == 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes == 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
+    let h = hours < 10 ? "0" + hours : hours;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let s = seconds < 10 ? "0" + seconds : seconds;
+
+    time.innerHTML = h + ":" + m + ":" + s;
+}
+
+function watchStart() {
+    if (timer !== null) {
+        clearInterval(timer);
+    }
+    timer = setInterval(stopwatch, 1000);
+}
+
+function watchStop() {
+    clearInterval(timer);
+}
+
+function watchRestart() {
+    clearInterval(timer);
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    time.innerHTML = "00:00:00";
+}
